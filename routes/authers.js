af507510cf21ed493ef19ae0f107bb34d70b035b
AgamalARM <agamalarm@gmail.com>
2024-03-07 07:19:26 +0200

    create update and delete api

--- routes/authers.js
+++ routes/authers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
 
-authers = [{
+const authers = [{
     id: 1,
     firstName: "ahmed",
     lastName: "gamal"
@@ -62,11 +62,59 @@ router.post("/", (req,res) => {
     res.status(201).json(auther);
 })
 
+/**
+ * @desc Update an auther by id
+ * @route /api/authers/:id
+ * @method PUT
+ * @access public
+ */
+ router.put("/:id", (req,res) => {
+    // validation of input user using Joi
+    const { error } = validateUpdateAuther(req.body);
+    if (error) {
+        return res.status(400).json({ message: error.details[0].message}); // 400 =>Bad Request
+    }
+
+    const book = books.find(b => b.id === parseInt(req.params.id)); // to convert req.params.id to integer
+    if(book){
+        res.status(200).json({ message: "Auther has been updated" });
+    }else{
+        res.status(404).json({ message: "Auther Not Found" });
+    }
+
+})
+
+/**
+ * @desc Delete an auther by id
+ * @route /api/authers/:id
+ * @method DELETE
+ * @access public
+ */
+ router.delete("/:id", (req,res) => {
+    const auther = authers.find(a => a.id === parseInt(req.params.id)); // to convert req.params.id to integer
+    if(auther){
+        res.status(200).json({ message: "Auther has been deleted" });
+    }else{
+        res.status(404).json({ message: "Auther Not Found" });
+    }
+
+})
+
+
 // function to validate create a book
 function validateCreateAuther(obj) {
     const schema = Joi.object({
         firstName: Joi.string().trim().min(3).max(200).required(),
         lastName:  Joi.string().trim().min(3).max(200).required()
+    })
+    return schema.validate(obj);
+    
+}
+
+function validateUpdateAuther(obj) {
+    const schema = Joi.object({
+        firstName: Joi.string().trim().min(3).max(200),
+        lastName:  Joi.string().trim().min(3).max(200)
     })
     return schema.validate(obj);
     
