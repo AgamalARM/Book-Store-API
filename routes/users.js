4045a03dca0f34c420b3631a2a862b6ea990d347
AgamalARM <agamalarm@gmail.com>
2024-04-09 05:06:30 +0200

    add users route and make update router

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
+const { User, validateUpdateUser} = require('../models/User');
 
 
 /**
@@ -10,7 +11,25 @@ const bcrypt = require('bcryptjs');
  * @method PUT
  * @access private
  */
-router.put(asyncHandler(async(req,res) => {
+router.put("/:id",asyncHandler(async(req,res) => {
+    const { error } = validateUpdateUser(req.body);
+    if(error){
+        return res.status(400).json({ message: error.details[0].message});
+    }
+    if(req.body.password){
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+
+    }
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        $set: {
+            email: req.body.email,
+            password: req.body.password,
+            userName: req.body.userName
+
+        }
+    }, { new: true}).select("-password");
+    res.status(200).json(updatedUser);
 
 }));
 
